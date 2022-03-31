c7f995a9c6b24ccd8b6945dfdd441e618abd1967
whisley0 <39428174+whisley0@users.noreply.github.com>
2022-03-31 09:31:25 +0800

    Update setup.sh
    rewrite as there is typo

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
-mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "edward_lam@vfc.com"n
+echo "\
+[general]\n\
+email = \"edward_lam@vfc.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
